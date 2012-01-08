14f96fc5e3705bb02d79f8c42285dbd5458a6aa2
orlando.schaefer <orlando.schaefer@smartbees.com>
2012-01-08 13:42:36 +0100

    - Major project settings for getting social integration working properly - Deleted SBJSON framework, now using JSONKit from sharekit

---
+++ Cookielicious/CLDefaultSHKConfigurator.m
@@ -0,0 +1,13 @@
+//
+//  CLDefaultSHKConfigurator.m
+//  Cookielicious
+//
+//  Created by Orlando Schäfer on 07.01.12.
+//  Copyright (c) 2012 cookcrowd. All rights reserved.
+//
+
+#import "CLDefaultSHKConfigurator.h"
+
+@implementation CLDefaultSHKConfigurator
+
+@end
