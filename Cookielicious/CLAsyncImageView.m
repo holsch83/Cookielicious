c52fc75ff22e6beb8476895ec53f88805c2d9aa7
orlando.schaefer <orlando.schaefer@smartbees.com>
2012-01-15 17:55:14 +0100

    Changed CLAsyncImage View: now using default image while loading

--- Cookielicious/CLAsyncImageView.m
+++ Cookielicious/CLAsyncImageView.m
@@ -24,30 +24,33 @@
   
   [request setDelegate:self];
   [request startAsynchronous];
+  [self setContentMode:UIViewContentModeCenter];
+  [self setImage:[UIImage imageNamed:@"image_default.png"]];
 }
 
 #pragma mark - ASIHTTPRequestDelegate
 
-- (void) requestStarted:(ASIHTTPRequest *)request {
-  NSLog(@"Requesting image started");
-  
-  _activityIndicatorView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
-  [_activityIndicatorView setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0.8]];
-  
-  UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
-  [indicator startAnimating];
-    
-  indicator.frame = CGRectMake((self.frame.size.width / 2) - (indicator.frame.size.width / 2), (self.frame.size.height / 2) - (indicator.frame.size.height / 2), indicator.frame.size.width, indicator.frame.size.height);
-  
-  [_activityIndicatorView addSubview:indicator];
-  [self addSubview:_activityIndicatorView];
-}
+//- (void) requestStarted:(ASIHTTPRequest *)request {
+//  NSLog(@"Requesting image started");
+//  
+//  _activityIndicatorView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+//  [_activityIndicatorView setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0.8]];
+//  
+//  UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+//  [indicator startAnimating];
+//    
+//  indicator.frame = CGRectMake((self.frame.size.width / 2) - (indicator.frame.size.width / 2), (self.frame.size.height / 2) - (indicator.frame.size.height / 2), indicator.frame.size.width, indicator.frame.size.height);
+//  
+//  [_activityIndicatorView addSubview:indicator];
+//  [self addSubview:_activityIndicatorView];
+//}
 
 - (void)requestFinished:(ASIHTTPRequest *)request {
   NSLog(@"Request finished");
   
   UIImage *theImage = [[UIImage alloc] initWithData:[request responseData]];
-  [super setImage:theImage];
+  [self setContentMode:UIViewContentModeScaleAspectFill];
+  [self setImage:theImage];
   
   [_activityIndicatorView removeFromSuperview];
 }
